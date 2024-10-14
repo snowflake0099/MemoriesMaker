b9299a0ee76ed59cb164dc2107570875f6ba2cfb
selmaAlzarrouk <80717479+selmaAlzarrouk@users.noreply.github.com>
2024-10-14 15:04:33 +0100

    http status codes 409, 201,200,401

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,17 +1,26 @@
 import PostMessage from "../models/postMessage.js";
 
-export const getPosts = async (req,res)=>{
-  try{
+// Function to get all posts
+export const getPosts = async (req, res) => {
+  try {
     const postMessages = await PostMessage.find();
-    // console.log(postMessages);
-    // res.status(200).json(postMessages);
-} catch (error){
-res.status(404).json({message: error.message});
+    res.status(200).json(postMessages);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
   }
-}
+};
 
-export const createPost = (req,res)=>{
-    res.send('Post Creation');
-}
+// Function to create a post
+export const createPost = async(req, res) => {
+    const post = req.body;
+    const newPost = new PostMessage(post);
+   
+    try{
+        await newPost.save();
+        res.status(201).json(newPost);
 
-export default postMessage; 
+    }catch(error){
+        res.status(409).json({message: error.message});
+
+    }
+};
